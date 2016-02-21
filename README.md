d054e9bca982911ce35059bf4889147aa57cde73
soef <soef@gmx.net>
2016-02-21 14:52:16 +0100

    Autodiscover and some optimizations

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ![Logo](admin/yamaha.png)
-# ioBroker.yamaha
+## ioBroker.yamaha
 
 Adapter for Yamaha AV receivers
 
@@ -20,12 +20,13 @@ npm install iobroker.yamaha
 
 #### Requirements
 Yamaha Reciver
+
 You have to enable "network standby" function in the configuration of your receiver
 
 ### License
 The MIT License (MIT)
 
-Copyright (c) 2015 soef <soef@gmx.net>
+Copyright (c) 2015-2016 soef <soef@gmx.net>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- yamaha.js
+++ yamaha.js
@@ -1,16 +1,64 @@
 "use strict";
 
 var utils = require(__dirname + '/lib/utils');
-var adapter = utils.adapter('yamaha');
+var soef = require(__dirname + '/lib/soef'),
+    devices = new soef.Devices();
 
-var YamahaAPI = require("yamaha-nodejs");
+//var YamahaAPI = require("yamaha-nodejs");
+var YAMAHA = require("yamaha-nodejs");
 var yamaha;
 
-function YAMAHA (ip) {
-    this.constructor(ip);
-}
+var adapter = utils.adapter({
+    name: 'yamaha',
 
-YAMAHA.prototype = new YamahaAPI();
+    unload: function (callback) {
+        try {
+            callback();
+        } catch (e) {
+            callback();
+        }
+    },
+    //discover: function (callback) {
+    //},
+    install: function (callback) {
+        log("adapter.on(install)");
+        adapter.getStatesOf(function (err, objs) {
+            if (objs) {
+                for (var i = 0; i < objs.length; i++) {
+                    log(objs[i]._id);
+                    adapter.setState(objs[i]._id, { val: "", ack: false });
+                }
+            }
+        });
+    },
+    //uninstall: function (callback) {
+    //},
+    //objectChange: function (id, obj) {
+    //    //adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
+    //},
+    stateChange: function (id, state) {
+        if (state && !state.ack) {
+            yamaha.execCommand(id, state.val);
+        }
+    },
+    ready: function () {
+        devices.init(adapter, function(err) {
+            main();
+        });
+    }
+});
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+//function YAMAHA (ip) {
+//    //this.constructor(ip);
+//    YamahaAPI.call(this, ip);
+//}
+
+//var YAMAHA = YamahaAPI;
+
+//YAMAHA.prototype = new YamahaAPI();
 YAMAHA.prototype.setMute = function (to) {
     var command = '<YAMAHA_AV cmd="PUT"><Main_Zone><Volume><Mute>' + (to ? "On" : "Off") + '</Mute></Volume></Main_Zone></YAMAHA_AV>';
     return this.SendXMLToReceiver(command);
@@ -24,7 +72,6 @@ YAMAHA.prototype.adjustVolume = function (dif) {
         adapter.setState("volume", { val: val, ack: true });
     });
 }
-
 
 YAMAHA.prototype.execCommand = function (id, val) {
     
@@ -81,124 +128,144 @@ YAMAHA.prototype.execCommand = function (id, val) {
     }
 }
 
-
-adapter.on('install', function () {
-    log("adapter.on(install)");
-    adapter.getStatesOf(function (err, objs) {
-        if (objs) {
-            for (var i = 0; i < objs.length; i++) {
-                log(objs[i]._id);
-                adapter.setState(objs[i]._id, { val: "", ack: false });
-            }
-        }
-    });
-});
-
-
-adapter.on('unload', function (callback) {
-    try {
-        adapter.log.info('cleaned everything up...');
-        callback();
-    } catch (e) {
-        callback();
-    }
-});
-
-
-adapter.on('objectChange', function (id, obj) {
-    // Warning, obj can be null if it was deleted
-    //adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
-});
-
-
-
-adapter.on('stateChange', function (id, state) {
-    adapter.log.debug('stateChange ' + id + ' ' + JSON.stringify(state));
-    
-    if (state && !state.ack) {
-        yamaha.execCommand(id, state.val);
-    }
-});
-
-
-adapter.on('message', function (obj) {
-    if (typeof obj == 'object' && obj.message) {
-        if (obj.command == 'send') {
-            // e.g. send email or pushover or whatever
-            console.log('send command');
-
-            // Send response in callback if required
-            if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-        }
-    }
-});
-
-
-adapter.on('ready', function () {
-    main();
-});
-
-
-function updateState(name, val) {
-    adapter.getState(name, function (err, obj) {
-        if (!obj || obj.val !== val)
-            adapter.setState(name, { val: val, ack: true });
-    });
-}
-
-
 function updateStates() {
     yamaha.getBasicInfo().done(function (v) {
-        adapter.log.debug ("getBasicInfo " + JSON.stringify(v));
-        updateState("input", v.getCurrentInput());           
-        updateState("volume", v.getVolume());
-        updateState("mute", v.isMuted());
-        updateState("power", v.isOn());
-        updateState("surround", v.YAMAHA_AV.Main_Zone[0].Basic_Status[0].Surround[0].Program_Sel[0].Current[0].Sound_Program[0]);
+        //adapter.log.debug ("getBasicInfo " + JSON.stringify(v));
+
+        var dev = new devices.CState('');
+        dev.set("input", v.getCurrentInput());
+        dev.set("volume", v.getVolume());
+        dev.set("mute", v.isMuted());
+        dev.set("power", v.isOn());
+        dev.set("surround", v.YAMAHA_AV.Main_Zone[0].Basic_Status[0].Surround[0].Program_Sel[0].Current[0].Sound_Program[0]);
+        dev.update();
 
 
-        if (adapter.config.intervall !== undefined && adapter.config.intervall !== 0) {
-            setTimeout(updateStates, adapter.config.intervall * 1000);
+        var intervall = adapter.config.intervall >> 0;
+        //if (adapter.config.intervall !== undefined && adapter.config.intervall !== 0) {
+        if (intervall) {
+            setTimeout(updateStates, intervall * 1000);
         }
     });
 }
 
+function repairConfig () {
+    if (!adapter.config['IP'] && !adapter.config['Intervall']) {
+        return;
+    }
+    adapter.getForeignObject("system.adapter." + adapter.namespace, function (err, obj) {
+        var changed = false;
+        if (obj.native['Intervall']) {
+            delete obj.native.Intervall;
+            if (!obj.native['intervall']) {
+                obj.native.intervall = 120;
+            }
+            changed = true;
+        }
+        if (obj.native['IP']) {
+            obj.native.ip = obj.native.IP;
+            delete obj.native.IP;
+            changed = true;
+        }
+        if (changed) {
+            //delete obj.native.Password;
+            //delete obj.native.pollingInterval;
+            //delete obj.native.User;
+            adapter.setForeignObject(obj._id, obj, {}, function (err, obj) {
+            });
+        }
+    });
+}
+
+function discoverReceiver(callback) {
+
+    function saveFoundIP(ip, callback) {
+        adapter.getForeignObject("system.adapter." + adapter.namespace, function (err, obj) {
+            obj.native.ip = ip;
+            adapter.setForeignObject(obj._id, obj, {}, function (err, obj) {
+                adapter.config.ip = ip;
+                callback();
+            });
+        });
+    }
+
+    require('dns').lookup(require('os').hostname(), function (err, add, fam) {
+        if (err || !add) {
+            return;
+        }
+        var ip = '';
+        var prefixIP = add.split('.', 3).join('.') + '.';
+        var request = require('./node_modules/yamaha-nodejs/node_modules/request');
+        for (var i = 1; i < 255; i++) {
+            if (ip) break;
+            request.post(
+                {
+                    timeout: 200,
+                    method: 'POST',
+                    uri: 'http://' + prefixIP + i + '/YamahaRemoteControl/ctrl',
+                    body: '<YAMAHA_AV cmd="GET"><System><Config>GetParam</Config></System></YAMAHA_AV>'
+                },
+                function (err, response, body) {
+                    if (!err && response.statusCode == 200) {
+                        ip = response.request.host;
+                        var r = body.match("<Model_Name>(.*?)</Model_Name>.*?<System_ID>(.*?)</System_ID>.*?<Version>(.*?)</Version>");
+                        r = r || body.match("<Model_Name>(.*?)</Model_Name>");
+                        if (r && r.length >= 4) {
+                            adapter.log.info('Yamaha Receiver found. IP: ' + ip + ' - Model: ' + r[1] + ' - System-ID: ' + r[2] + ' - Version: ' + r[3]);
+                        } else if (r && r.length >= 2) {
+                            adapter.log.info('Yamaha Receiver found. IP: ' + ip + ' - Model: ' + r[1]);
+                        }
+                        saveFoundIP(ip, callback);
+                    }
+                }
+            );
+        }
+    });
+}
+
+function checkIP(callback) {
+    if (adapter.config.ip) {
+        callback();
+        return;
+    }
+    discoverReceiver(callback);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function main() {
-    
-    yamaha = new YAMAHA(adapter.config.ip);
-    
-    adapter.getStatesOf(function (err, objs) {
-        if (objs) {
-            for (var i = 0; i < objs.length; i++) {
-                updateState(objs[i]._id, 0);
-            }
-        }
-        updateStates();
-        
-        adapter.subscribeStates('*');
-    });
-    
 
-    //yamaha.getWebRadioList().done(function (v) {
-    //    console.log(JSON.stringify(v));
-    //});
-    //yamaha.getList("NET_RADIO").done(function (v) {
-    //    console.log(JSON.stringify(v));
-    //});
-    
-    yamaha.getAvailableInputs().done(function (v) {
-        adapter.log.debug("getAvailableInputs: " + JSON.stringify(v));
-        var inputs = v;
-        adapter.getObject("input", function (err, obj) {
-            obj.native.values = inputs + [,"AirPlay", "NET_RADIO", "Napster", "Spotify"];
-            adapter.setObject("input", obj);
-        })
-    });
-    
-    yamaha.getSystemConfig().done(function (v) {
-        adapter.log.debug("getSystemConfig: " + JSON.stringify(v));
-        updateState("SystemConfig.name", v.YAMAHA_AV.System[0].Config[0].Model_Name[0]);
-        updateState("SystemConfig.version", v.YAMAHA_AV.System[0].Config[0].Version[0]);
+    repairConfig();
+    checkIP(function() {
+        yamaha = new YAMAHA(adapter.config.ip);
+
+        updateStates();
+
+        adapter.subscribeStates('*');
+
+        //yamaha.getWebRadioList().done(function (v) {
+        //    console.log(JSON.stringify(v));
+        //});
+        //yamaha.getList("NET_RADIO").done(function (v) {
+        //    console.log(JSON.stringify(v));
+        //});
+
+        yamaha.getAvailableInputs().done(function (v) {
+            adapter.log.debug("getAvailableInputs: " + JSON.stringify(v));
+            var inputs = v;
+            adapter.getObject("input", function (err, obj) {
+                obj.native.values = inputs + [, "AirPlay", "NET_RADIO", "Napster", "Spotify"];
+                adapter.setObject("input", obj);
+            })
+        });
+
+        yamaha.getSystemConfig().done(function (v) {
+            adapter.log.debug("getSystemConfig: " + JSON.stringify(v));
+            var dev = new devices.CState('SystemConfig');
+            dev.set("name", v.YAMAHA_AV.System[0].Config[0].Model_Name[0]);
+            dev.set("version", v.YAMAHA_AV.System[0].Config[0].Version[0]);
+            dev.update();
+        });
     });
 }
